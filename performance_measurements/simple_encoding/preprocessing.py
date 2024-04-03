6cd0c6e4cea2f8cc686c24a66936ae24d97f0c18
Eduard Stan <edigeorges50@gmail.com>
2024-04-03 20:24:36 +0300

    performed grid search for all models in simple_encoding preprocessing folder. Created a second csv file in which we store the macro f1 scores. Created another way to display the results. Updated the comments.

--- performance_measurements/simple_encoding/preprocessing.py
+++ performance_measurements/simple_encoding/preprocessing.py
@@ -2,14 +2,16 @@
 import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score, accuracy_score, make_scorer, classification_report
+from sklearn.model_selection import GridSearchCV
 
 
 # define the constants of the file.
-accuracies_path = '../../conclusions/accuracies.csv'  # the file in which we draw our results
+f1_scores_path = '../../conclusions/f1_scores_macro.csv'  # the file in which we draw the F1 scores
+accuracies_path = '../../conclusions/accuracies.csv'  # the file in which we draw the accuracies
 raw_data = pd.read_csv('../../data/diabetic_data.csv')  # the file from which we extract the data
 
-def write_conclusions(model, accuracy):
+def write_conclusions(model, f1_score, accuracy):
     """
     Draws the results into the files in which we store the conclusions of this project.
     The results are displayed at:
@@ -20,14 +22,20 @@ def write_conclusions(model, accuracy):
     # we store the name of the current folder. It symbolizes the preprocessing method we're using
     folder_name = os.getcwd().split('/')[-1]  
     model_name = type(model).__name__  # the name of the model
-    accuracies_df = pd.read_csv(accuracies_path)  # load the accuracies data into a pd.DataFrame
+    f1_df = pd.read_csv(f1_scores_path)  # load the macro F1 scores data into a pd.DataFrame
+    acc_df = pd.read_csv(accuracies_path)  # load the accuracies data into a pd.DataFrame
 
-    # extract the ROW index for the accuracy metric, according to the preprocessing method we're using
-    accuracies_row_index = accuracies_df.loc[accuracies_df['PreparationMethod'] == folder_name].index[0]
+    # extract the ROW index for the macro F1 score and accuracy metrics, according to the preprocessing method we're using
+    f1_row_index = f1_df.loc[f1_df['PreparationMethod'] == folder_name].index[0]
+    acc_row_index = acc_df.loc[acc_df['PreparationMethod'] == folder_name].index[0]
+
     # we draw the results at coordinates ROW and COLUMN
-    accuracies_df.loc[accuracies_row_index, model_name] = accuracy
+    f1_df.loc[f1_row_index, model_name] = f1_score
+    acc_df.loc[acc_row_index, model_name] = accuracy
+    
     # dump the updated data into the corresponding csv file
-    accuracies_df.to_csv(accuracies_path, index=False) 
+    f1_df.to_csv(f1_scores_path, index=False) 
+    acc_df.to_csv(accuracies_path, index=False)
 
 
 def encode_object_features(df):
@@ -47,3 +55,6 @@ encoded_data = encode_object_features(raw_data)
 # Extract features and labels and then split them into train and test data
 X, y = encoded_data.drop(columns=['readmitted']), encoded_data['readmitted']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Select the scoring metric as being the macro F1 score
+scoring_metric = make_scorer(f1_score, average='macro')
